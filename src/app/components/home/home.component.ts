88f072cb8284a1eff8a00f5f6aeb632063bb1022
Marcos <marcosmuller87@gmail.com>
2022-07-25 08:50:39 -0300

    Aplicativo finalizado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  edad = 25;
+  peso = 60;
+  altura = 170;
+  sexo = 'M';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  mudarAltura(event: any) {
+    this.altura = event.target.value;
   }
 
+  masculino() {
+    this.sexo = 'M';
+  }
+
+  femenino() {
+    this.sexo = 'F';
+  }
+
+  calcularBMI() {
+    const BMI = this.peso / Math.pow(this.altura / 100,2);
+    this.router.navigate(['/resultado', BMI.toFixed(1)]);
+  }
 }
--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
   templateUrl: './resultado.component.html',
-  styleUrls: ['./resultado.component.css']
+  styleUrls: ['./resultado.component.css'],
 })
 export class ResultadoComponent implements OnInit {
+  bmi: number;
+  resultado: string;
+  interpretacao: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.resultado = '';
+    this.interpretacao = '';
+    this.bmi = +route.snapshot.paramMap.get('valor')!;
   }
 
+  ngOnInit(): void {
+    this.getResultado();
+  }
+
+  getResultado() {
+    if (this.bmi < 18.5) {
+      this.resultado = 'Abaixo do peso';
+      this.interpretacao = 'Você está abaixo do peso';
+    }
+    if (this.bmi < 25 && this.bmi >= 18.5) {
+      this.resultado = 'Peso ideal';
+      this.interpretacao = 'Você está no peso ideal';
+    }
+    if (this.bmi >= 25 && this.bmi < 30) {
+      this.resultado = 'Sobrepeso';
+      this.interpretacao =
+        'Você está acima do peso, tente seguir um regime de alimentação mais equilibrado';
+    }
+
+    if (this.bmi >= 30 && this.bmi < 35) {
+      this.resultado = 'Obesidade grau I';
+      this.interpretacao =
+        'Você esta com obesidade grau I, procure um profissional para realizar um exame mais detalhado';
+    }
+    if (this.bmi >= 35 && this.bmi < 40) {
+      this.resultado = 'Obesidade grau II';
+      this.interpretacao =
+        'Você está com obesidade grau II, procure um profissional para realizar um exame mais detalhado';
+    }
+    if (this.bmi >= 40) {
+      this.resultado = 'Obesidade grau III';
+      this.interpretacao =
+        'Você está com obesidade grau III, procure um profissional para realizar um exame mais detalhado';
+    }
+  }
 }
